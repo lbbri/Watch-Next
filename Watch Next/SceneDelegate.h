4aa79bd448cc1921af78094dd5c819c6c8412e97
Briana Mason <brm14@fb.com>
2020-07-14 08:27:34 -0700

    Initial Commit

---
+++ Watch Next/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Watch Next
+//
+//  Created by brm14 on 7/14/20.
+//  Copyright © 2020 Mason Creations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Watch Next/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Watch Next
+//
+//  Created by brm14 on 7/14/20.
+//  Copyright © 2020 Mason Creations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
