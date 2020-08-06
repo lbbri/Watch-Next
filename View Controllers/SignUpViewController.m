b9b309b8db1ab2b07428b999c7f733f59713752f
Briana Mason <brm14@fb.com>
2020-08-06 13:27:26 -0700

    Sign Up page styled with Material library.

--- View Controllers/SignUpViewController.m
+++ View Controllers/SignUpViewController.m
@@ -10,18 +10,34 @@
 #import "WatchNextUser.h"
 #import <Parse/Parse.h>
 
-@interface SignUpViewController ()
+#import <MaterialTextFields.h>
+#import <MaterialButtons.h>
+#import "MaterialButtons+ButtonThemer.h"
+#import "MDCButton+MaterialTheming.h"
 
-@property (weak, nonatomic) IBOutlet UITextField *usernameField;
-@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@interface SignUpViewController () <UITextFieldDelegate>
+
+@property (weak,nonatomic) IBOutlet MDCTextField *nameField;
+@property (weak, nonatomic) IBOutlet MDCTextField *usernameField;
+@property (weak, nonatomic) IBOutlet MDCTextField *passwordField;
+@property (weak, nonatomic) IBOutlet MDCTextField *retypeField;
+@property (weak, nonatomic) IBOutlet MDCButton *signUpButton;
+
+
+@property(nonatomic) MDCTextInputControllerOutlined *nameController;
+@property(nonatomic) MDCTextInputControllerOutlined *usernameController;
+@property(nonatomic) MDCTextInputControllerOutlined *passwordController;
+@property(nonatomic) MDCTextInputControllerOutlined *retypeController;
 
 @end
 
-@implementation SignUpViewController
+@implementation SignUpViewController 
 
 - (void)viewDidLoad {
     
     [super viewDidLoad];
+    
+    [self setUpVisuals];
 }
 /**
  @brief Allows the user to create a new Watch Next account with a username and password.
@@ -60,6 +76,52 @@
             }
         }];
     }
+}
+
+#pragma mark - Visual Polish
+
+- (void) setUpVisuals {
+    
+    self.nameField.placeholder = @"Name";
+    self.usernameField.placeholder = @"Username";
+    self.passwordField.placeholder = @"Password";
+    self.retypeField.placeholder = @"Retype Password";
+    
+    
+    self.nameController = [[MDCTextInputControllerOutlined alloc] initWithTextInput:self.nameField];
+    self.usernameController = [[MDCTextInputControllerOutlined alloc] initWithTextInput:self.usernameField];
+    self.passwordController = [[MDCTextInputControllerOutlined alloc] initWithTextInput:self.passwordField];
+    self.retypeController = [[MDCTextInputControllerOutlined alloc] initWithTextInput:self.retypeField];
+    
+    self.passwordField.delegate = self;
+    self.retypeField.delegate = self;
+    
+    MDCContainerScheme *containerScheme = [[MDCContainerScheme alloc] init];
+    containerScheme.colorScheme.primaryColor = UIColor.lightGrayColor;
+    
+    [self.signUpButton applyContainedThemeWithScheme: containerScheme];
+    [self.signUpButton setTitle:@"Sign Up" forState:UIControlStateNormal];
+    self.signUpButton.minimumSize = CGSizeMake(64, 36);
+    CGFloat verticalInset = MIN(0, (CGRectGetHeight(self.signUpButton.bounds) - 48) / 2);
+    self.signUpButton.hitAreaInsets = UIEdgeInsetsMake(verticalInset, 0, verticalInset, 0);
+    
+
+}
+
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+  [textField resignFirstResponder];
+
+  // Text Field Validation
+  if (textField == (UITextField *)self.retypeField) {
+      if (![self.retypeField.text isEqualToString:self.passwordField.text]) {
+       [self.retypeController setErrorText:@"Passwords Must Match" errorAccessibilityValue:nil];
+    } else {
+       [self.retypeController setErrorText:nil errorAccessibilityValue:nil];
+    }
+  }
+
+  return NO;
 }
 
 
